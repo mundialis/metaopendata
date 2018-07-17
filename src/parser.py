ac5f19e4a238ad91c4472612bd6442e0aca66f8f
Jan Orzekowsky <orzekowsky@terrestris.de>
2018-07-17 15:21:05 +0200

    parse urls

--- src/parser.py
+++ src/parser.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python
 
+import re
+
 hostname = 'db'
 username = 'postgres'
 password = '6Hwg8a7z3m7TZMg6'
@@ -8,12 +10,17 @@ database = 'bmvimetadaten'
 def doQuery( conn ) :
     cur = conn.cursor()
 
-    cur.execute( "SELECT id, data FROM metadata" )
+    cur.execute( "SELECT id, data FROM metadata WHERE data LIKE '%WFS%'" )
 
     i = 0
     for id, data in cur.fetchall() :
         i += 1
         print (i, id, data[:100])
+        # url = '<p>Hello World</p><a href="http://example.com">More Examples</a><a href="http://example2.com">Even More Examples</a>'
+
+        urls = re.findall('https?://(?:[-\w.]|(?:%[\da-fA-F]{2}))+', data)
+
+        >>> print urls
 
 
 import psycopg2
