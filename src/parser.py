78c8d475f363475a3af72cbaf54d66955dce7b0a
Jan Orzekowsky <orzekowsky@terrestris.de>
2018-07-19 17:12:52 +0200

    added python library + parser now able to identify and download gml urls

--- src/parser.py
+++ src/parser.py
@@ -1,25 +1,36 @@
 #!/usr/bin/python
 
-# import re
-import xml.etree.ElementTree as ET
+# import xml.etree.ElementTree as ET
+import lxml.etree as ET
+import urllib.request
 
 hostname = 'db'
 username = 'postgres'
 password = '6Hwg8a7z3m7TZMg6'
 database = 'bmvimetadaten'
 
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
 def doQuery( conn ) :
     cur = conn.cursor()
 
-    cur.execute( "SELECT id, data FROM metadata WHERE data LIKE '%SERVICE=WFS%'" )
-    # cur.execute( "SELECT id, data FROM metadata WHERE id = 370" )
+    # cur.execute( "SELECT id, data FROM metadata WHERE data LIKE '%SERVICE=WFS%'" )
+    cur.execute( "SELECT id, data FROM metadata WHERE id IN ( 370, 218, 319, 3814, 4208, 1552, 2377, 1202, 2530)" )
 
     i = 0
     for id, data in cur.fetchall() :
         i += 1
         root = ET.fromstring( data )
         # urls = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', data)
-        print( i, id )
+        # print( "# WFS Num + id :", i, id )
 
         # print ( root )
         # for elem in root.iter() :
@@ -38,15 +49,68 @@ def doQuery( conn ) :
         # for child2 in child.findall( 'distributionInfo' ) :
         #     for child3 in child2:
         #         print(child3.tag, child3.attrib)
-        elements = root.findall(".//{http://www.isotc211.org/2005/gmd}URL")
+        wfsUrls = root.findall( ".//{*}URL" )
+        print (  )
+        print ( bcolors.UNDERLINE + bcolors.HEADER + "#### " + str(i) + " ####" + bcolors.ENDC )
         # print ( elements )
-        for elem in elements :
-            if "SERVICE=WFS" in elem.text:
-                print ( elem.text )
+        for wfsUrl in wfsUrls :
+            if "REQUEST=GetCapabilities" in wfsUrl.text and "SERVICE=WFS" in wfsUrl.text:
+                wfsName = wfsUrl.text.split('?')[0].split('/')[-1]
 
-                #1 take GetCapabilities url -> get xml
-                #2 find <FeatureTypeList> <FeatureType> <Name> -> build getFeature URL -> dl GML file
-                #3 write all in db
+                print ( bcolors.BOLD + "### " + str(id) + " " + wfsName + " ###" + bcolors.ENDC )
+                print ( bcolors.OKBLUE + "## " + wfsUrl.text + " ##" + bcolors.ENDC )
+
+#1 take GetCapabilities url -> get xml
+######################################
+
+                response = urllib.request.urlopen( wfsUrl.text )
+                # xmlString = response.read().decode()
+                # xml = ET.ElementTree( ET.fromstring( xmlString ) )
+                xml = ET.fromstring( response.read() )
+                # print ( xml )
+                # for elem in xml.iter() :
+                #      print ( elem.tag, elem.text )
+
+#2 find <FeatureTypeList> <FeatureType> <Name>
+##############################################
+
+                # featureTypes = xml.findall( ".//{http://www.opengis.net/wfs/2.0}FeatureType" )
+                # featureTypes = xml.findall( ".//{http://www.opengis.net/wfs/2.0}Name" )
+                featureTypes = xml.findall( ".//{*}FeatureType/{*}Name" )
+                if not featureTypes :
+                    print ( bcolors.FAIL + "# No FeatureTypeName found (check parser)!" + bcolors.ENDC )
+                for featureType in featureTypes :
+                    print ( bcolors.OKGREEN + "# FeatureTypeName : " + featureType.text + bcolors.ENDC )
+
+#4 build FeatureType url
+########################
+                    # verison ??? --> ServiceTypeVersion
+                    versions = xml.findall( ".//{*}ServiceTypeVersion" )
+                    for version in versions :
+                        # print ( version.text )
+                        # replace REQUEST=GetCapabilities with REQUEST=GetFeature&typeNames=FeatureTypeName&count=5
+                        gmlUrl = wfsUrl.text.replace( "REQUEST=GetCapabilities" , "version=" + version.text + "&REQUEST=GetFeature&count=5&typeNames=" + featureType.text )
+                        # print ( gmlUrl )
+
+#5 download GML
+###############
+                        response = urllib.request.urlopen( gmlUrl )
+                        # gml = ET.fromstring( response.read() )
+                        # print ( gml )
+                        fileName = wfsName + "-" + featureType.text.replace( ":", "-" ) + "-" + version.text + ".gml"
+                        text_file = open( "/download/" + fileName, "wb" )
+                        text_file.write( response.read() )
+                        text_file.close()
+                        print ( "  saved file: " + fileName )
+#3 write all in db
+##################
+# CREATE TABLE gml_files (
+#     id          integer,
+#     metadata_id integer,
+#     filename    varchar(40) NOT NULL,
+#     created     date,
+#     PRIMARY KEY (id)
+# );
 
 
 import psycopg2
