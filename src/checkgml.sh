bad435a746ddae4a595de8df9760ae22acd0d247
Jan Orzekowsky <orzekowsky@terrestris.de>
2018-08-22 14:30:03 +0200

    check gml files

---
+++ src/checkgml.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+GML_DIR=$1
+
+# echo "GML_DIR: ${GML_DIR}"
+
+#### check if gml-file is valid
+for filepath in ${GML_DIR}/*.gml; do
+  filename=$(basename ${filepath})
+  # echo "filename: ${filename}"
+  docker run -v ${GML_DIR}:/data geodata/gdal ogrinfo /data/${filename} > /dev/null
+  if [ $? -ne 0 ]; then
+    echo -e "\e[31mFAIL\e[0m ${filename}"
+  else
+    echo -e "\e[32mOK\e[0m ${filename}"
+  fi
+done
